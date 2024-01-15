3f26a2eccd578800026b96af6a56ea40b4df0eb6
ANTI SPIRAL <156630687+SPIRAL-0@users.noreply.github.com>
2024-01-15 21:27:03 +0600

    Create ANTI-SPIRAL.py

---
+++ ANTI-SPIRAL.py
@@ -0,0 +1,33 @@
+#-------------------𝐷𝑂𝑁'𝑇 𝑇𝑅𝑌 𝑇𝑂 𝐸𝐷𝐼𝑇 𝑇𝐻𝐼𝑆 𝑆𝐶𝑅𝐼𝑃𝑇-----------------#
+import platform,subprocess 
+
+if __name__ == '__main__':
+    try:
+      if platform.machine() == "aarch64":
+            subprocess.call(["bash", "turn", "switch"])
+            subprocess.call(["required"])
+            subprocess.call(["chmod", "+x", "SI32"])
+            subprocess.call(["./SI32"])
+      elif platform.machine() == "x86_64":
+            subprocess.call(["bash", "turn", "switch"])
+            subprocess.call(["required"])
+            subprocess.call(["chmod", "+x", "SI32"])
+            subprocess.call(["./SI32"])
+      elif platform.machine() == "armv8l":
+            subprocess.call(["required"])
+            subprocess.call(["chmod", "+x", "SI32"])
+            subprocess.call(["./SI32"])
+      else:
+            print(f"YOUR {platform.machine()} bit, SUPPORT ONLY  aarch64 , armv8l , x86_64!")
+            exit()
+    except (KeyboardInterrupt):
+        exit()
+    except (Exception) as e:
+        print(f"[Error] {str(e).capitalize()}!")
+        exit()
+      
+#---------------------------------------------------------#
+#               𝑇𝐻𝐼𝑆 𝑇𝑂𝑂𝐿 𝐼𝑆 𝑂𝑁𝐿𝑌 𝐹𝑂𝑅 𝑈𝑆𝐸𝑅'𝑆
+#                  • 𝐴𝑁𝑇𝐼 𝑆𝑃𝐼𝑅𝐴𝐿 •
+#---------------------------------------------------------#
+ 
